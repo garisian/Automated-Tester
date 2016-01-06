a902416c6cf0d8764ce27c88391e7ec60ca31ba5
Garisian Kana <garisian22@gmail.com>
2016-01-06 15:02:16 -0500

    implemented working locks

--- run_tests.pl
+++ run_tests.pl
@@ -1,20 +1,47 @@
 #!/usr/bin/perl
 
-#########################################
-# Include all packages that we're using #
-#########################################
-
-#use RuggedCom::LogToFile;
-
-
-
 ###################################################
 # Make sure multiple tests aren't running at once #
 ###################################################
 
+# If lock already exists, that means another test set is currently being run
 my $lock_file = "lock";
 if(-e $lock_file)
 {
     print "Script is terminated. Another test is currently running under same directory\n";
     exit 0;
+}
+
+# Get the current time so we can update the lock file
+$datestring =localtime();
+
+# Update lock file with current time so in case of a program crash, we know when the set was run
+open(my $concurrency_lock, ">", $lock_file);
+print $concurrency_lock "Test initiated at $datestring\n";
+close($concurrency_lock);
+
+
+
+###################################
+# Include all packages being used #
+###################################
+
+#
+
+
+##########################################
+# Extraction and Execution of Test Cases #
+##########################################
+
+# At this point test cases are finished, so we can remove lock and terminate script
+remove_lock();
+
+#########################
+# Function Declarations #
+#########################
+
+# Remove the lock after test case finishes and before script terminates
+sub remove_lock
+{
+    `rm -f $lock_file`;
 }
